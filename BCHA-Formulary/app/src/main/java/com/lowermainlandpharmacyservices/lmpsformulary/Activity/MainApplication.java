bcd33294ee1227db6dc95c7ae07c7f4bd452eaee
Kelvin Chan <chan.kelv@gmail.com>
2016-06-12 03:16:23 -0700

    Added gradle for new firebase

--- BCHA-Formulary/app/src/main/java/com/lowermainlandpharmacyservices/lmpsformulary/Activity/MainApplication.java
+++ BCHA-Formulary/app/src/main/java/com/lowermainlandpharmacyservices/lmpsformulary/Activity/MainApplication.java
@@ -2,8 +2,6 @@ package com.lowermainlandpharmacyservices.lmpsformulary.Activity;
 
 import android.app.Application;
 
-import com.firebase.client.Firebase;
-
 /**
  * Created by kelvinchan on 2016-04-30.
  */
@@ -12,6 +10,5 @@ public class MainApplication extends Application {
     @Override
     public void onCreate(){
         super.onCreate();
-        Firebase.setAndroidContext(this);
     }
 }
